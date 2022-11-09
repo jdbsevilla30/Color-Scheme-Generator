1ecf86074023726e1cb2a2b30be50e82b38aabc3
jdbsevilla30 <98524643+jdbsevilla30@users.noreply.github.com>
2022-11-10 01:37:13 +0800

    the color array can now be displayed on the serial

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
-fetch("https://www.thecolorapi.com/scheme?hex=EFEFEF&count=6")
-  .then((res) => res.json())
-  .then((data) => {
-    data.colors.forEach(function (color) {
-      const { clean, value } = color.hex;
-      console.log(value)
-      //console.log(color);
-      //  console.log(color.hex.clean);
+const submitBtn = document.getElementById("submit");
+
+let colorsArray = [];
+
+submitBtn.addEventListener("submit", function (e) {
+  e.preventDefault();
+  colorsArray = [];
+  let colorer = document.getElementById("kulay").value;
+  let type = document.getElementById("scheme-mode").value;
+  let colored = colorer.replace("#", ""); //seed
+  fetch(
+    `https://www.thecolorapi.com/scheme?hex=${colored}&mode=${type}&count=4`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      colorsArray.push(colored);
+      data.colors.forEach(function (color) {
+        const { clean, value } = color.hex;
+        // console.log(clean); //hex
+        colorsArray.push(clean);
+      });
     });
-  });
+  console.log(type); //input list
+  console.log(colorsArray);
+});
