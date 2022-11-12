ca6c0da3eaa03e35a440b72f5f5d1eab0c527641
jdbsevilla30 <98524643+jdbsevilla30@users.noreply.github.com>
2022-11-13 02:56:45 +0800

    Added necessary styling

--- index.js
+++ index.js
@@ -1,54 +1,40 @@
-let colorsArray = []; //where we will store our seed and our generated colors
+let isPressed = false; //safety net so we don't f up our work
 const colorSeed = document.getElementById("get-seed"); //seed value
-let isPressed = false;
-let htmlString = ``;
-
 document.getElementById("submit").addEventListener("submit", function (e) {
   e.preventDefault();
   if (!isPressed) {
     render();
   }
 });
-
 const render = () => {
   //call reset so our DOM and array won't be overflowed
-  emptyElements();
+  let colorsArray = [];
+  let htmlString = ``;
+  let colorString = ``;
+  document.body.style.backgroundColor = colorSeed.value;
   const schemeMode = document.getElementById("scheme-mode").value;
   const colorContainer = document.getElementById("color-container"); //color container
+  const colorNameContainer = document.getElementById("color-name-container");
   const seed = colorSeed.value.replace("#", ""); //we must remove the # because the hex doesnt accept #
-
   fetch(
     `https://www.thecolorapi.com/scheme?hex=${seed}&mode=${schemeMode}&count=4`
   )
     .then((res) => res.json())
     .then((data) => {
+      console.log(data);
       colorsArray.push(colorSeed.value.toUpperCase());
       data.colors.forEach(function (color) {
-        const { value /**clean */ } = color.hex;
-        colorsArray.push(value); //colorsArray.push(clean) no # code
+        colorsArray.push(color.hex.value); //colorsArray.push(clean) no # code
       });
       isPressed = false;
       colorsArray.map((colorScheme) => {
-        htmlString += ` 
-      
-        <div class="color-flex" style="background-color: ${colorScheme}">color</div>
-        `;
-        console.log(htmlString);
+        htmlString += `<div class="color" style="background-color: ${colorScheme}"></div>`;
+        colorString += `<h1 class="color-label">${colorScheme}</h1>`;
       });
       colorContainer.innerHTML = htmlString;
+      colorNameContainer.innerHTML = colorString;
     });
 };
+colorSeed.value = "#" + Math.floor(Math.random() * 16777215).toString(16);
 
-const generateColor = () => {
-  colorSeed.value = "#" + Math.floor(Math.random() * 16777215).toString(16);
-};
-
-const emptyElements = () => {
-  //resets the DOM and empties the array
-  colorsArray = [];
-  htmlString = ``;
-  isPressed = true;
-};
-
-generateColor();
 render();
